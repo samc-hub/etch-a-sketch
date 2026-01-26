8e7982022aaf975d0390bb5765ce5c1c2de3366c
samc-hub <samuelcox2343@gmail.com>
2026-01-26 00:13:27 -0800

    Create a grid of divs with js controlled column count.

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const container = document.querySelector('#container');
 
 function createGrid (rows, columns) {
     const container = document.querySelector('#container');
-    const allBoxes = (rows * columns);
-    for (let i = 0; i <= allBoxes ; i ++) {
+    container.style.setProperty('--columns', columns);
+    const rowsColumns= (rows * columns);
+    for (let i = 0; i <= (rowsColumns) - 1 ; i ++) {
         const sketchBox = document.createElement('div');
         sketchBox.classList.add("sketchBox");
+       // sketchBox.style.width = `calc(${boxWidth} - 2px)`; //
         container.appendChild(sketchBox);
     }
 }
-createGrid(16,16);
+createGrid(4,4);
