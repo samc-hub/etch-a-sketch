01be76eb3fb63f1c37e102af3fe58e7c59b917a2
samc-hub <samuelcox2343@gmail.com>
2026-01-27 22:41:11 -0800

    Simplify user prompt to ask for one number
    Clear grid on click and generate the correct
    number of rows and columns

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const container = document.querySelector('#container');
 const button = document.querySelector('#newGrid');
-button.addEventListener("click", () => {
-        let newRows = prompt("How Many Rows?");
-        let newColumns = prompt("How Many Columns");
-        let parsedRow = Number(newRows);
-        let parsedColumns = Number(newColumns);
-        if ((parsedRow < 100)&&(parsedColumns < 100)) {
-            createGrid(parsedRow, parsedColumns);
+    button.addEventListener("click", () => {
+        let rowCol = prompt("How Many Squares Per Side?");
+        let numbrRowCol = Number(rowCol);
+        if (rowCol< 100) {
+            createGrid(numbrRowCol, numbrRowCol);
         };
 });
-
+function refreshPage () {
+    window.location.reload();
+}
 
 function createGrid (rows, columns) {
     const container = document.querySelector('#container');
+    container.innerHTML = '';
     container.style.setProperty('--columns', columns);
     const rowsColumns= (rows * columns);
     for (let i = 0; i <= (rowsColumns) - 1 ; i ++) {
@@ -21,9 +22,21 @@ function createGrid (rows, columns) {
         sketchBox.addEventListener('mouseenter', () => {
             sketchBox.classList.add("draw");
 
-        })
+        });
         container.appendChild(sketchBox);
-    }
-}
+    };
+};
 
+const randomColor = (() => {
+    "use strict";
+    const randomInt = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+    return () => {
+        var h = randomInt(0, 360);
+        var s = randomInt(42, 98); 
+        var l = randomInt(40, 90);
+        return `hsl(${h},${s},${l}%)`;
+    };
+})();
 
